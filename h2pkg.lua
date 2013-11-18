c5a440f4254fc3b52ccf17df952bd6d0871ec00e
iWoz <wuageek@163.com>
2013-11-18 20:44:07 +0800

    Fix h2pkg.lua.

--- h2pkg.lua
+++ h2pkg.lua
@@ -1,16 +1,39 @@
 require( "OSUtil", package.seeall )
 --set your path, default current path.
-local path = ""
+local path = "D:/Cocos2D-X/TorchLight/newcode/extensions/CocoStudio"
+--local path = "D:/Cocos2D-X/TorchLight/newcode/extensions/CocoStudio/GUI"
+--local path = "D:/Cocos2D-X/TorchLight/newcode/cocos2dx/support/component"
+--local path = "D:/Cocos2D-X/TorchLight/newcode/extensions/network"
 --set your output path, default current path "__pkgs__" dir.
 local outputPath = ""
+--pkgs you don't wanna change for special reasons
+local exceptList = 
+{
+	CCBone=1,
+	ColliderBody=1,
+	CCSkin=1,
+	CCSGUIReader=1,
+	CCSSceneReader=1,
+	CCDatas=1,
+	json_batchallocator=1,
+	json_tool=1,
+	reader=1,
+	value=1,
+	UISwitch=1,
+	UIWidget=1,
+}
 
 local SKIP_MODE = 0
 local WRITE_MODE = 1
+
+local NO_BRACE = 0
+local IN_BRACE = 1
 
 local function classResolve( cls )
 	local cont = ""
 	local decalr = nil
 	local publicBlock = nil
+	local brace = NO_BRACE
 	local mode = SKIP_MODE
 	string.gsub( cls, "(.-)\r?\n", function(line)
 		--2.remove CC_DLL and multi inherites
@@ -24,26 +47,43 @@ local function classResolve( cls )
 		end
 		local function writePublicBlock()
 			if publicBlock and string.find( publicBlock, "%(.*%)" ) then
+				publicBlock = string.gsub( publicBlock, "/%*.-%*/", "")
 				cont = cont..publicBlock.."\n"
 			end
 			publicBlock = nil
 		end
+		--for CocoStudio
+		line = string.gsub( line, "const char %*string", "const char %*stringValue" )
 		--3.remove public protect and private
 		--4.remove the decalration of class member variable
 		--5.remove member functions declared as private or protected
-		if line == "public:" and mode == SKIP_MODE then
+		--print( mode, brace )
+		if string.match(line,"%s*public:%s*") and mode == SKIP_MODE then
 			mode = WRITE_MODE
-		elseif mode == WRITE_MODE and (line == "private:" or line == "protected:" or line == "};") then
+		elseif mode == WRITE_MODE and brace == IN_BRACE and string.match(line,"%s*};%s*") then
+			brace = NO_BRACE
+			publicBlock = publicBlock..";\n"
+		elseif mode == WRITE_MODE and (string.match(line,"%s*private:%s*") or string.match(line,"%s*protected:%s*") or (string.match(line,"%s*};%s*") and not string.match(line,"%s*{.-};%s*")) ) then
 			writePublicBlock()
 			mode = SKIP_MODE
-		elseif mode == WRITE_MODE and line == "public:" then
+		elseif mode == WRITE_MODE and string.match(line,"%s*public:%s*") then
 			writePublicBlock()			
 		end
-		if line ~= "public:" and mode == WRITE_MODE then
+		--print( line, mode, brace )
+		if not string.match(line,"%s*public:%s*") and not string.match(line,"%s*};%s*") and mode == WRITE_MODE then
 			if publicBlock == nil then
 				publicBlock = ""
 			end
-			publicBlock = publicBlock..line.."\n"
+			--remove the function body defined in header
+			line = string.gsub( line, "{.-};", ";" )
+			--remove inline keyword for declaration and implementation
+			local inline = string.match( " "..line, "%s*[^%S]inline[^%S]%s*" )
+			if string.match( line, "{" ) then
+				brace = IN_BRACE
+			end
+			if brace == NO_BRACE and not string.match(line, "%s*//.-%s") and not inline then
+				publicBlock = publicBlock..line..(string.match(line, ";") and "\n" or "")
+			end
 		end
 	end  )
 	cont = cont.."\n};\n"
@@ -52,13 +92,18 @@ end
 
 local function getPKGContent( header )
 	local content = ""
+	--0.remove all comments
+	header = string.gsub( header, "/%*.-%*/", "")
 	--1.enum keeps the same
-	string.gsub( header, "(enum.-}.-;)", function(enum)
-		if string.match( enum, "}%w+;" ) then
+	string.gsub( header, "(enum[^%S].-}.-;)", function(enum)
+		--print( enum )
+		if string.match( enum, "}%s*%w+%s*;" ) then
 			enum = "typedef "..enum
 		end
 		content = content .. enum .. "\n\n"
 	end )
+	--remove declared class header
+	header = string.gsub( header, "%s*class%s%w-%s-;", "")
 	--get class content
 	string.gsub( header, "(\nclass.-{.-\n};\n)", function(cls)
 		content = content .. classResolve( cls ) .. "\n\n"
@@ -80,16 +125,18 @@ local pkgContent
 local pkg
 for i = 1,#headers do
 	fullFileName = headers[i]
-	filename = string.match( fullFileName, ".+"..OSUtil.getOSSp().."(%w-)%.h" )
-	--print( fullFileName, filename )
-	header = io.open( fullFileName, "r+" )
-	content = header:read("*a")
-	pkgContent = getPKGContent( content )
-	if pkgContent and pkgContent ~= "" then
-		io.close(header)
-		pkg = io.open(outputPath.."/"..filename..".pkg", "w+")
-		pkg:write( pkgContent )
-		io.close( pkg )
-		print('$pfile "'..filename..'.pkg"')
+	filename = string.match( fullFileName, ".+"..OSUtil.getOSSp().."(.-)%.h" )
+	if not exceptList[filename] then
+		--print( fullFileName, filename )
+		header = io.open( fullFileName, "r+" )
+		content = header:read("*a")
+		pkgContent = getPKGContent( content )
+		if pkgContent and pkgContent ~= "" then
+			io.close(header)
+			pkg = io.open(outputPath.."/"..filename..".pkg", "w+")
+			pkg:write( pkgContent )
+			io.close( pkg )
+			print('$pfile "'..filename..'.pkg"')
+		end
 	end
 end
