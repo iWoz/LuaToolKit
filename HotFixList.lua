6a8f4ddb3342e1d2d43a97da518eba791f3754af
iWoz <wuzw@soulgame.com>
2015-06-30 23:51:43 +0800

    ...

--- HotFixList.lua
+++ HotFixList.lua
@@ -1,4 +1,4 @@
-HotFixList = 
 {
-	{v="1.0.1", u="http://wuzhiwei.net/xx.zip", s = 123},
+	{v="1.0.1", u="http://wuzhiwei.net/1.0.1.zip", s = 123},
+	{v="1.0.2", u="http://wuzhiwei.net/1.0.2.zip", s = 321},
 }
